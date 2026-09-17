049ae23238882f7ce61dbd799d2112daa8b3e840
Pierre Jaen <peterjaen09@gmail.com>
2026-09-18 00:05:09 +0200

    musica 2

--- script.js
+++ script.js
@@ -26,3 +26,57 @@ document.addEventListener("click", function iniciarAudio() {
         }).catch(() => {});
     }
 }, { once: true });
+
+
+
+const supabaseUrl = "https://rluqpkxzfjcxjqjmfrrj.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJsdXFwa3h6ZmpjeGpxam1mcnJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk2NzAyNjAsImV4cCI6MjEwNTI0NjI2MH0.GfxqlUPxsmOoQA7tTTzVjl2DFalGfXrr7Bklmpou2lc";
+
+const client = supabase.createClient(
+    supabaseUrl,
+    supabaseKey
+);
+
+async function guardarCancion(){
+
+    const song =
+        document.getElementById("song").value;
+
+    if(!song.trim()){
+        alert("Escribe una canción");
+        return;
+    }
+
+    const { error } =
+        await client
+        .from("song_requests")
+        .insert([
+            {
+                song: song
+            }
+        ]);
+
+    if(error){
+        document.getElementById("status").innerText =
+        "❌ Error al guardar";
+        return;
+    }
+
+    document.getElementById("status").innerText =
+    "✅ Canción enviada";
+
+    document.getElementById("song").value = "";
+}
+
+function toggleMusica() {
+    const audio = document.getElementById("bg-music");
+    const btn = document.getElementById("music-toggle");
+
+    if (audio.paused) {
+        audio.play();
+        btn.innerText = "🔊 Pausar música";
+    } else {
+        audio.pause();
+        btn.innerText = "🎵 Reproducir música";
+    }
+}
